adcd5b99590116e9da2ca5167829607dabfde2a3
jererc <jererc@gmail.com>
2024-09-10 13:02:27 +0400

    add linux support

--- bfshuffle.py
+++ bfshuffle.py
@@ -1,38 +1,71 @@
-import os.path
+import os
 import random
 import subprocess
 import time
+from urllib.parse import urlparse
 
 from selenium import webdriver
 from selenium.common import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
 
 
-CHROME_PROFILE_PATH = os.path.expanduser(
+CHROME_WIN_DATA_DIR = os.path.expanduser(
     r'~\AppData\Local\Google\Chrome\User Data')
+CHROME_LINUX_DATA_DIR = os.path.expanduser(
+    '~/.config/google-chrome')
+IS_NT = os.name == 'nt'
+DATA_DIR = CHROME_WIN_DATA_DIR if IS_NT else CHROME_LINUX_DATA_DIR
+DEFAULT_PROFILE_DIR = 'selenium'
 MAX_MAPS = 20
 
 
 class BFShuffler(object):
 
-    def __init__(self):
+    def __init__(self, profile_dir=DEFAULT_PROFILE_DIR):
+        self.profile_dir = profile_dir
         self.driver = self._get_driver()
 
 
     def _get_driver(self):
-        if not os.path.exists(CHROME_PROFILE_PATH):
-            raise Exception(f'chrome profile path {CHROME_PROFILE_PATH}'
-                ' does not exist')
-        subprocess.call('taskkill /IM chrome.exe', shell=True)
+        data_dir = CHROME_WIN_DATA_DIR if IS_NT else CHROME_LINUX_DATA_DIR
+        if not os.path.exists(data_dir):
+            raise Exception(f'chrome data dir {data_dir} does not exist')
+        subprocess.call('taskkill /IM chrome.exe' if IS_NT else 'pkill chrome',
+            shell=True)
         options = Options()
-        options.add_argument(f'--user-data-dir={CHROME_PROFILE_PATH}')
+        options.add_argument(f'--user-data-dir={data_dir}')
+        options.add_argument(f'--profile-directory={self.profile_dir}')
+        options.add_argument('--start-maximized')
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        options.add_experimental_option('detach', True)
         driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(1)
-        driver.switch_to.window(driver.window_handles[-1])
         return driver
+
+
+    def _validate_url(self, url):
+        if not urlparse(url).path.endswith('/choose-maps'):
+            raise Exception('invalid url (expects '
+                'https://portal.battlefield.com/experience/mode/choose-maps?playgroundId=<UUID>)')
+
+
+    def _wait_for_login(self, url, poll_frequency=1, timeout=120):
+        self._validate_url(url)
+        self.driver.get(url)
+        end_ts = time.time() + timeout
+        while time.time() < end_ts:
+            try:
+                return self.driver.find_element(By.XPATH,
+                    '//div[@title and contains(., "CORE")]')
+            except NoSuchElementException:
+                time.sleep(poll_frequency)
+        raise Exception('login timeout')
 
 
     def _find_map_elements(self):
@@ -41,13 +74,13 @@ class BFShuffler(object):
 
     def _wait_for_maps(self, url, attempts=3, poll_frequency=1, timeout=5):
         for i in range(attempts):   # sometimes the content does not load
-            self.driver.get(url)
             end_ts = time.time() + timeout
             while time.time() < end_ts:
                 if self._find_map_elements():
                     return
                 time.sleep(poll_frequency)
-        raise Exception('timeout')
+            self.driver.get(url)
+        raise Exception('maps timeout')
 
 
     def _clear_maps(self):
@@ -81,8 +114,15 @@ class BFShuffler(object):
         )
 
 
+    def _add_map_element(self, element):
+        element.find_element(By.XPATH,
+            './/button[mat-icon[@data-mat-icon-name="add_circle_outline"]]',
+            ).click()
+
+
     def shuffle(self, url, included_maps=None, excluded_maps=None,
             max_maps=MAX_MAPS):
+        self._wait_for_login(url)
         self._wait_for_maps(url)
         self._clear_maps()
         map_els = self._find_map_elements()   # needs to be reloaded to avoid stale elements
@@ -94,12 +134,18 @@ class BFShuffler(object):
         y_offset = map_els[0].location['y']
         for name in selected_maps:
             data = map_data[name]
-            y_delta = data['y'] - y_offset
-            if y_delta:
-                self._scroll_from_element(map_els[0], y_delta)
-            data['el'].find_element(By.XPATH,
-                './/button[mat-icon[@data-mat-icon-name="add_circle_outline"]]',
-                ).click()
+            try:
+                self._add_map_element(data['el'])
+            except ElementClickInterceptedException:
+                y_delta = data['y'] - y_offset
+                if y_delta:
+                    self._scroll_from_element(map_els[0], y_delta)
+                    try:
+                        self._add_map_element(data['el'])
+                    except ElementClickInterceptedException:
+                        print(f'failed to add map {name}')
+
         self.driver.find_element(By.XPATH,
             '//button[@aria-label="save button"]').click()
         time.sleep(3)
+        self.driver.quit()

b0478a2a1379fcf302e93fc70651329dc98bc9dd
jererc <jererc@gmail.com>
2024-11-20 12:13:04 +0400

    update

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,3 +1,5 @@
 @echo off
 curl -s "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/bootstrap.py" | python
-curl -O "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/user_settings.py"
+if not exist "user_settings.py" (
+    curl -O "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/user_settings.py"
+)
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 curl -s "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/bootstrap.py" | python3
-curl -O "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/user_settings.py"
+if [ ! -e "user_settings.py" ]; then
+    curl -O "https://raw.githubusercontent.com/jererc/bfshuffle/refs/heads/main/bootstrap/user_settings.py"
+fi
